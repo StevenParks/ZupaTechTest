9cf7942470a6ea95c0a5e887364f7c10e5972955
Steven Parks <steven@mparks.co.uk>
2018-06-30 10:40:32 +0100

    Add project files.

---
+++ zupaTechTest/Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace zupaTechTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        //Retrieve all Unbooked Seats
+
+        //Attempt to Book all Seat/Name/Email combinations in an array
+    }
+}
